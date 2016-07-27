eda95fa7648685f186c4dc2594becef0ee688fd1
Alex <mckibbenta@gmail.com>
2016-07-26 22:47:56 -0400

    Added hyperlink reference modes
    Removed interpreted text as it was interfering with the reference modes
    and wasn't highlighting anything anyway. Might attempt to re-implement
    once the solution is more sophisticated.

--- src/languages/rst.js
+++ src/languages/rst.js
@@ -15,16 +15,17 @@ function(hljs) {
                 className: "strong",
                 begin: /\*{2}[^*]/, end: /\*{2}/
             },
-            // Interpreted text is generally not highlighted. However,
-            // including the mode might help relevancy and allow users to
-            // implement custom styling if they wish.
-            {
-                className: "interpreted-text",
-                begin: /`{1}[^`]/, end: /`/
-            },
             {
                 className: "literal",
                 begin: /`{2}[^`]/, end: /`{2}/
+            },
+            // hyperlink references
+            {
+                className: "symbol",
+                variants: [
+                    { begin: /[a-zA-Z0-9-_.:+]+_{1,2}\b/ },
+                    { begin: /`[a-zA-Z0-9-_.:+ ]+`_{1,2}\b/ }
+                ]
             }
         ]
     };
