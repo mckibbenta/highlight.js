b23b893be69c45510748af4a1886326caf9f56b9
Alex <mckibbenta@gmail.com>
2016-07-26 20:09:50 -0400

    Added interpreted text mode.
    This mode uses a non-standard class name and so will not be highlighted
    by default.

--- src/languages/rst.js
+++ src/languages/rst.js
@@ -13,6 +13,13 @@ function(hljs) {
             {
                 className: "strong",
                 begin: /\*{2}[^*]/, end: /\*{2}/
+            },
+            // Interpreted text is generally not highlighted. However,
+            // including the mode might help relevancy and allow users to
+            // implement custom styling if they wish.
+            {
+                className: "interpreted-text",
+                begin: /`{1}[^`]/, end: /`/
             }
         ]
     };
